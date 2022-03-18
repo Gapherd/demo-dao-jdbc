4de53f77d530b8ab02bfff62177ecfeb84fe8d45
Gabriel Mendes <gabriel.mendes@ciss.com.br>
2022-03-18 13:03:18 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -8,6 +11,10 @@ public class Program {
 
         Department obj = new Department(1, "books");
         System.out.println(obj);
+
+        Seller seller = new Seller(1, "Gabriel", "gabriel@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 
 }
