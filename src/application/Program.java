167b1de822e5028d9502de3efc12f8aea7fea65c
Gabriel Mendes <gabriel.mendes@ciss.com.br>
2022-03-18 20:01:35 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
         Seller newSeller = new Seller(null, "Gabriela Souza", "gabriela@gmail.com", new Date(), 4500.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = "+newSeller.getId());
+
+        System.out.println("===seller update===");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Aparecida");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
     }
 
 }
